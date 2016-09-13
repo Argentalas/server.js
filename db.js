61f31d23ba9f3491c9bf92e689b09f51396c2cbc
mks <msnitkovski@gmail.com>
2016-09-13 16:25:05 +0300

    coded async db.read()

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 //db.js
-
 const fs = require('fs');
+const EE = require('events');
+
+const utl = require('./utl.js');
 
 db.write = write;
 db.read = read;
@@ -10,7 +12,7 @@ module.exports = db;
 ///////////////////////
 
 function db() {
-	// Reserved for sql
+	// Reserved for queries to sql dbms
 }
 
 function write(rel, pkey, value) {
@@ -18,15 +20,32 @@ function write(rel, pkey, value) {
 };
 
 function read(rel, pkey) {
-	//
 
-	if (!rel) {
-		
+	var result = new EE();
+
+	// If no arguments provided return list of relations (a.k.a. tables)
+	if (!arguments.length) {
+		fs.readdir('data', (err,files)=>{
+			if (err) {
+				utl.log(err);
+				result.emit('done', err);
+			} else {
+				result.emit('done', files);
+			}
+		});
+		return result;
 	};
 
-	fs.readFile(`data/${rel}.json`, (err,data)=>{
-
-	})
-
+	fs.readFile(`data/${rel}.json`, (err, d)=>{
+		if (err) {
+			utl.log(err);
+			result.emit('done', err);
+		} else if (arguments.length < 2){
+			result.emit('done', JSON.parse(d));
+		} else {
+			result.emit('done', JSON.parse(d)[pkey]);
+		};
+	});
+	
+	return result;
 };
-
