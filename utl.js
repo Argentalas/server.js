a34de44a778655e70bbd6353da82cece109756c7
mks <msnitkovski@gmail.com>
2016-09-13 11:56:41 +0300

    minor update

--- utl.js
+++ utl.js
@@ -40,7 +40,7 @@ function mime(filePath) {
 
 function rerequire(modname) {
 
-	// require() caches, hence this function.
+	// require() caches and never invalidates, hence this function.
 	// Using on native addons will result in an Error. https://nodejs.org/dist/latest-v4.x/docs/api/globals.html#globals_require_cache
 	// It is absolutely fine to require smth for the first time with this function.
 	// Keep in mind, there is a syncronous file system read under the hood.
