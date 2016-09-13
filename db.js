753af234fdf686ca719d1600a6a54a39c01ac1b5
mks <msnitkovski@gmail.com>
2016-09-13 18:34:55 +0300

    rewrote db module

--- db.js
+++ db.js
@@ -1,19 +1,33 @@
 //db.js
 const fs = require('fs');
-const EE = require('events');
 
 const utl = require('./utl.js');
-
-db.write = write;
-db.read = read;
 
 module.exports = db;
 
 ///////////////////////
 
-function db() {
-	// Reserved for queries to sql dbms
+function db(rel, pkey, value) {
+
+	// If no arguments provided return list of relations (a.k.a. tables)
+	var l = fs.readdirSync('data');
+	if (arguments.length === 0) {return l};
+
+	// If only one argument provided return entire table
+	if (l.indexOf(rel+'.json') === -1) {return 'table not found'};
+	var r = JSON.parse(fs.readFileSync(`data/${rel}.json`));
+	if (arguments.length === 1) {return r};
+
+	// If two arguments provided return requested value
+	if (arguments.length === 2) {return r[pkey] || 'entry not found'};
+
+	// If at least three arguments provided then write provided value to the database
+	r[pkey] = value;
+	fs.writeFileSync(`data/${rel}.json`, JSON.stringify(r));
 }
+
+//This is a relic and will be deleted
+//////////////////////////////////////////
 
 function write(rel, pkey, value) {
 	
@@ -28,7 +42,7 @@ function read(rel, pkey) {
 		fs.readdir('data', (err,files)=>{
 			if (err) {
 				utl.log(err);
-				result.emit('done', err);
+				result.emit('error', err);
 			} else {
 				result.emit('done', files);
 			}
@@ -37,15 +51,19 @@ function read(rel, pkey) {
 	};
 
 	fs.readFile(`data/${rel}.json`, (err, d)=>{
+		
 		if (err) {
 			utl.log(err);
-			result.emit('done', err);
+			result.emit('error', err);
 		} else if (arguments.length < 2){
+		
+			// If no pkey provided return entire object
 			result.emit('done', JSON.parse(d));
+		
 		} else {
-			result.emit('done', JSON.parse(d)[pkey]);
+			result.emit('done', JSON.parse(d)[pkey] || '');
 		};
 	});
-	
+
 	return result;
 };
