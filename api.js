ad40e89617ce4f5bd0da824798ef99f8c5cb46f3
mks <msnitkovski@gmail.com>
2016-09-20 18:40:22 +0300

    started AAA functionality

--- api.js
+++ api.js
@@ -1,9 +1,28 @@
+//api.js
+var bcrypt = require('bcrypt');
+
 const utl = require('./utl.js');
 
 module.exports = api;
 
+///////////////////////////
+
 function api(req, res) {
 	var command = utl.parseurl(req.url)[1];
-	var token = utl.parseurl(req.url)[2];
+	var username = utl.parseurl(req.url)[2];
 
+};
+
+function authenticate(token, req) {
+
+	bcrypt.compare(token)
+};
+
+function authorize(command, username) {
+	try {
+		return command in db.sec(users, username).permissions;
+	} catch (e){
+		utl.log(e);
+		return false;
+	};
 };
