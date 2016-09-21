ab7aac2740984862f4391c7c2696a5e36436d16d
mks <msnitkovski@gmail.com>
2016-09-21 18:06:37 +0300

    comment edit

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 //db.js
-
 const fs = require('fs');
 
 var db = createDB('data');
@@ -32,22 +31,3 @@ function createDB(dbname) {
 		fs.writeFileSync(`${dbname}/${rel}.json`, JSON.stringify(r));
 	};
 };
-
-// function secure(rel, pkey, value) {
-
-// 	// If no arguments provided return list of relations (a.k.a. tables :-) )
-// 	var l = fs.readdirSync('private');
-// 	if (arguments.length === 0) {return l};
-
-// 	// If only one argument provided return entire table
-// 	if (l.indexOf(rel+'.json') === -1) {return 'table not found'};
-// 	var r = JSON.parse(fs.readFileSync(`private/${rel}.json`));
-// 	if (arguments.length === 1) {return r};
-
-// 	// If two arguments provided return requested value
-// 	if (arguments.length === 2) {return r[pkey] || 'entry not found'};
-
-// 	// If at least three arguments provided then write provided value to the database
-// 	r[pkey] = value;
-// 	fs.writeFileSync(`private/${rel}.json`, JSON.stringify(r));
-// };
