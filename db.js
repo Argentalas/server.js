711f0ead3a88d970afeeca9308d0a4f76536a669
mks <msnitkovski@gmail.com>
2016-09-21 12:21:28 +0300

    refactored the code, added internal functionality for creating databases

--- db.js
+++ db.js
@@ -2,46 +2,52 @@
 
 const fs = require('fs');
 
-db.sec = secure;
+var db = createDB('data');
+db.private = createDB('private');
 
 module.exports = db;
 
 ///////////////////////
 
-function db(rel, pkey, value) {
+function createDB(dbname) {
+	
+	fs.statSync(dbname).isDirectory() || fs.mkdir(dbname);
 
-	// If no arguments provided return list of relations (a.k.a. tables :-) )
-	var l = fs.readdirSync('data');
-	if (arguments.length === 0) {return l};
+	return	function (rel, pkey, value) {
 
-	// If only one argument provided return entire table
-	if (l.indexOf(rel+'.json') === -1) {return 'table not found'};
-	var r = JSON.parse(fs.readFileSync(`data/${rel}.json`));
-	if (arguments.length === 1) {return r};
+		// If no arguments provided return list of relations (a.k.a. tables :-) )
+		var l = fs.readdirSync(dbname);
+		if (arguments.length === 0) {return l};
 
-	// If two arguments provided return requested value
-	if (arguments.length === 2) {return r[pkey] || 'entry not found'};
+		// If only one argument provided return entire table
+		if (l.indexOf(rel+'.json') === -1) {return 'table not found'};
+		var r = JSON.parse(fs.readFileSync(`${dbname}/${rel}.json`));
+		if (arguments.length === 1) {return r};
 
-	// If at least three arguments provided then write provided value to the database
-	r[pkey] = value;
-	fs.writeFileSync(`data/${rel}.json`, JSON.stringify(r));
+		// If two arguments provided return requested value
+		if (arguments.length === 2) {return r[pkey] || 'entry not found'};
+
+		// If at least three arguments provided then write provided value to the database
+		r[pkey] = value;
+		fs.writeFileSync(`${dbname}/${rel}.json`, JSON.stringify(r));
+	};
 };
 
-function secure(rel, pkey, value) {
+// function secure(rel, pkey, value) {
 
-	// If no arguments provided return list of relations (a.k.a. tables :-) )
-	var l = fs.readdirSync('private');
-	if (arguments.length === 0) {return l};
+// 	// If no arguments provided return list of relations (a.k.a. tables :-) )
+// 	var l = fs.readdirSync('private');
+// 	if (arguments.length === 0) {return l};
 
-	// If only one argument provided return entire table
-	if (l.indexOf(rel+'.json') === -1) {return 'table not found'};
-	var r = JSON.parse(fs.readFileSync(`private/${rel}.json`));
-	if (arguments.length === 1) {return r};
+// 	// If only one argument provided return entire table
+// 	if (l.indexOf(rel+'.json') === -1) {return 'table not found'};
+// 	var r = JSON.parse(fs.readFileSync(`private/${rel}.json`));
+// 	if (arguments.length === 1) {return r};
 
-	// If two arguments provided return requested value
-	if (arguments.length === 2) {return r[pkey] || 'entry not found'};
+// 	// If two arguments provided return requested value
+// 	if (arguments.length === 2) {return r[pkey] || 'entry not found'};
 
-	// If at least three arguments provided then write provided value to the database
-	r[pkey] = value;
-	fs.writeFileSync(`private/${rel}.json`, JSON.stringify(r));
-};
+// 	// If at least three arguments provided then write provided value to the database
+// 	r[pkey] = value;
+// 	fs.writeFileSync(`private/${rel}.json`, JSON.stringify(r));
+// };
