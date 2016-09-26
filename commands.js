dbde226a89b41b6673dd17448313911d7cf1231b
mks <msnitkovski@gmail.com>
2016-09-26 17:35:50 +0300

    minor refactoring

--- commands.js
+++ commands.js
@@ -3,14 +3,19 @@
 // Add your commands here.
 // Core api methods are defined in api.js module.
 
-module.exports = {
+var commands = {};
 
-	echo: function(request, response){
+commands.echo = echo;
 
-		// request.msg property contains data sent to api.
-		var msg = request.msg;
+module.exports = commands;
 
-		//response.send(data) method sends back json; you can also use require('./utl.js').send(data, response) or full functionality of http.ServerResponse https://nodejs.org/dist/latest-v4.x/docs/api/http.html#http_class_http_serverresponse
-		response.send(msg.text);
-	}
+/////////////////////////////
+
+function echo(request, response){
+
+	// request.msg property contains data sent to api.
+	var msg = request.msg;
+
+	//response.send(data) method sends back json; you can also use require('./utl.js').send(data, response) or full functionality of http.ServerResponse https://nodejs.org/dist/latest-v4.x/docs/api/http.html#http_class_http_serverresponse
+	response.send(msg.text);
 };
