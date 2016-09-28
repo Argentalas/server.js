c1d2122764248d118829db00eccb12131fc0a6bf
mks <msnitkovski@gmail.com>
2016-09-28 18:46:36 +0300

    minor refactoring

--- commands.js
+++ commands.js
@@ -14,8 +14,8 @@ module.exports = commands;
 function echo(request, response){
 
 	// request.msg property contains data sent to api.
-	var msg = request.msg;
+	var text = request.msg.text;
 
 	//response.send(data) method sends back json; you can also use require('./utl.js').send(data, response) or full functionality of http.ServerResponse https://nodejs.org/dist/latest-v4.x/docs/api/http.html#http_class_http_serverresponse
-	response.send(msg.text);
+	response.send(text);
 };
